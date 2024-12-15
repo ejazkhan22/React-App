79c71bcaa6a6310cce225ea8a7ea32187cee4bd2
Ejaz khan <79781448+ejazkhan22@users.noreply.github.com>
2024-12-15 13:47:32 +0500

    Add project files

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+<>
+<h1>React App</h1>
+<img src="https://tse3.mm.bing.net/th?id=OIP.hFgK4loB5TNfgiwbE3ht1gHaDt&pid=Api&P=0&h=220img" alt="image" srcset="" />
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, libero repudiandae ea eaque mollitia minima architecto adipisci officiis. Blanditiis beatae eligendi, reprehenderit reiciendis dolorem sapiente quisquam facere est non facilis quas? In accusantium animi facilis explicabo neque id eius enim, temporibus atque non? Commodi, velit? Voluptatem doloribus, quam nemo non autem velit aliquid consequuntur alias assumenda deserunt animi blanditiis aliquam labore! Deserunt nam veniam enim quaerat officia quidem, fugiat expedita sapiente! Quo alias dolor maiores natus eligendi, officiis ipsum fuga sint culpa voluptatibus doloribus expedita, molestiae in suscipit sapiente porro. Dolores aut nam incidunt ipsum
+   velit ea libero temporibus natus.
+</p>
+</>
+)
